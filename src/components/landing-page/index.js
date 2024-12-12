db7c5b297fe37e70d98f1245fa88b1e6b96ed608
HoangTien268 <hoangxuantien268@gmail.com>
2024-12-12 15:27:28 +0700

    update title

--- src/components/landing-page/index.js
+++ src/components/landing-page/index.js
@@ -6,19 +6,19 @@ const LandingPage = () => {
   return (
     <>
       <div className="container-fluid p-0">
-        <div className="d-flex justify-content-between align-items-center p-3 fixed-top bg-white shadow-sm">
+        <div className="d-flex justify-content-between align-items-center p-3 fixed-top bg-black shadow-sm text-white">
           <div className="logo">LOGO</div>
           <nav className="nav">
-            <a href="#home" className="nav-link text-dark">
+            <a href="#home" className="nav-link text-white">
               Home
             </a>
-            <a href="#case-studies" className="nav-link text-dark">
+            <a href="#case-studies" className="nav-link text-white">
               Case studies
             </a>
-            <a href="#team" className="nav-link text-dark">
+            <a href="#team" className="nav-link text-white">
               Team
             </a>
-            <a href="#contact" className="nav-link text-dark">
+            <a href="#contact" className="nav-link text-white">
               Contact
             </a>
           </nav>
@@ -30,21 +30,29 @@ const LandingPage = () => {
         >
           <div className="overlay"></div>
           <div className="title ">
-            <h1 className="display-1 heroTitle fw-bold">
-              Introduce your{" "}
-              <span className="highlight text-success">idea.</span>
+            <h1 className="display-2 heroTitle fw-bold">
+              TechJ – Delivering Quality{" "}
+              <span className="highlight text-success">
+                Software Solutions.
+              </span>
             </h1>
-            <p className="display-5 fw-normal mt-4">Add your title.</p>
+            <p className="fs-4 fw-normal mt-4 " style={{ padding: "0 20%" }}>
+              TechJ chuyên phát triển phần mềm ứng dụng AI, giúp doanh nghiệp
+              tối ưu hóa quy trình và tăng cường hiệu suất một cách thông minh
+              và hiệu quả.
+            </p>
             <div className="cta mt-3 rounded mt-4">
               <button className="btn btn-success btn-lg">Call to action</button>
             </div>
           </div>
         </div>
 
-        <div className="features text-center" style={{ padding: "30px 10%" }}>
+        <div
+          className="features text-center text-white"
+          style={{ padding: "30px 10%" }}
+        >
           <h2 className="fs-1 fw-bold p-5">
-            Use this space to elaborate on your headline and connect with{" "}
-            <span className="highlight text-success">your visitors.</span>
+            Tại sao chọn <span className="highlight text-success">TechJ?</span>
           </h2>
           <div className="row p-3">
             <div className="col-md-4 featureItem">
@@ -53,10 +61,9 @@ const LandingPage = () => {
                 alt="Feature 1"
                 className="img-fluid featureImage mb-3"
               />
-              <h3 className="featureTitle">Add your title</h3>
+              <h3 className="featureTitle">Chất lượng vượt trội</h3>
               <p className="featureDescription">
-                Use this space to elaborate on your headline and connect with
-                your visitors...
+                Chúng tôi luôn đảm bảo về chất lượng của sản phẩm.
               </p>
             </div>
             <div className="col-md-4 featureItem">
@@ -65,10 +72,12 @@ const LandingPage = () => {
                 alt="Feature 2"
                 className="img-fluid featureImage mb-3"
               />
-              <h3 className="featureTitle">Add your title</h3>
+              <h3 className="featureTitle">
+                Đội ngũ chuyên môn cao, giàu kinh nghiệm
+              </h3>
               <p className="featureDescription">
-                Use this space to elaborate on your headline and connect with
-                your visitors...
+                Đội ngũ có năng lực và kinh nghiệm, luôn sẵn sàng giải quyết mọi
+                thách thức.
               </p>
             </div>
             <div className="col-md-4 featureItem">
@@ -77,10 +86,9 @@ const LandingPage = () => {
                 alt="Feature 3"
                 className="img-fluid featureImage mb-3"
               />
-              <h3 className="featureTitle">Add your title</h3>
+              <h3 className="featureTitle">Giải pháp tối ưu</h3>
               <p className="featureDescription">
-                Use this space to elaborate on your headline and connect with
-                your visitors...
+                Công nghệ linh hoạt, phù hợp với mọi mô hình.
               </p>
             </div>
           </div>
@@ -110,11 +118,14 @@ const LandingPage = () => {
           </div>
         </div>
 
-        <div className=" text-center" style={{ padding: "30px 10%" }}>
+        <div
+          className=" text-center text-white"
+          style={{ padding: "30px 10%" }}
+        >
           <div>
             <h2 className="fs-1 fw-bold p-5">
-              Use this space to elaborate on your headline and connect with{" "}
-              <span className="highlight text-success">your visitors.</span>
+              Dịch vụ của{" "}
+              <span className="highlight text-success">TechJ</span>
             </h2>
             <div className="images  mb-4">
               <div className="d-flex gap-5 text-align-center justify-content-between align-items-center">
@@ -123,21 +134,19 @@ const LandingPage = () => {
                   src="https://via.placeholder.com/300"
                   alt="Placeholder"
                 />
-                <p className="paragraph">
-                  Use this space to elaborate on your headline and connect with
-                  your visitors. You can build on the first idea or add your
-                  second point. Now that you've got your visitors' attention,
-                  let them know what they can expect, and encourage them to
-                  explore your site.
+                <p className="paragraph text-white">
+                  Chúng tôi chuyên phát triển các phần mềm ứng dụng trí tuệ nhân
+                  tạo, giúp tối ưu hóa quy trình kinh doanh.Đáp ứng các nhu cầu
+                  đặc thù của doanh nghiệp bằng các giải pháp AI được thiết kế
+                  riêng, mang lại hiệu quả và tính cạnh tranh cao.
                 </p>
               </div>
               <div className="d-flex gap-5 text-align-center justify-content-between align-items-center">
-                <p className="paragraph">
-                  Use this space to elaborate on your headline and connect with
-                  your visitors. You can build on the first idea or add your
-                  second point. Now that you've got your visitors' attention,
-                  let them know what they can expect, and encourage them to
-                  explore your site.
+                <p className="paragraph text-white">
+                  Hỗ trợ doanh nghiệp tiết kiệm nguồn lực và thời gian với các
+                  sản phẩm AI chất lượng cao, triển khai nhanh chóng.Đảm bảo hệ
+                  thống luôn được cập nhật công nghệ mới nhất, hoạt động ổn định
+                  và hiệu quả lâu dài.
                 </p>
                 <img
                   className="img-fluid image"
@@ -158,24 +167,25 @@ const LandingPage = () => {
           }}
         >
           <h2 className="mb-4 display-6 fw-bold">
-            Use this space to elaborate on your headline and connect with{" "}
-            <span className="highlight text-success">your visitors.</span>
+          <span className="highlight text-success">Techj</span>
+           – Đồng hành cùng doanh nghiệp trong hành trình tối ưu hóa và đổi mới với các giải pháp gia công phần mềm AI hàng đầu.{" "}
+            
           </h2>
           <div className="ctaButton2 fs-5 mt-4">Call to action</div>
         </div>
 
         <div className="navbar mt-5" style={{ margin: "30px 10%" }}>
           <div className="d-flex gap-2">
-            <a href="#home" className="nav-link text-dark">
+            <a href="#home" className="nav-link text-white">
               Home
             </a>
-            <a href="#case-studies" className="nav-link text-dark">
+            <a href="#case-studies" className="nav-link text-white">
               Case studies
             </a>
-            <a href="#team" className="nav-link text-dark">
+            <a href="#team" className="nav-link text-white">
               Team
             </a>
-            <a href="#contact" className="nav-link text-dark">
+            <a href="#contact" className="nav-link text-white">
               Contact
             </a>
           </div>
